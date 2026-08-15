465c8d0c907b9b513581d4049b6dde600f1f432d
Maria <mariiatrokoz@gmail.com>
2026-08-15 19:41:54 +0200

    created func for embedding model, filled config, added source

--- src/config.py
+++ src/config.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 # --- LLM Model Configuration ---
 LLM_MODEL: str = "llama-3.3-70b-versatile"
 LLM_SYSTEM_PROMPT: str = ("You talk in simple terms about complex topics.")
@@ -6,4 +8,23 @@ LLM_TEMPERATURE: float = 0.01
 LLM_TOP_P: float = 0.95
 LLM_REPETITION_PENALTY: float = 1.03
 
-CHAT_MEMORY_TOKEN_LIMIT = 1000
+CHAT_MEMORY_TOKEN_LIMIT = 4000
+
+# --- Embedding Model Configuration ---
+EMBEDDING_MODEL_NAME: str = "sentence-transformers/all-MiniLM-L6-v2"
+
+
+# --- RAG/VectorStore Configuration ---
+# The number of most relevant text chunks to retrieve from the vector store
+SIMILARITY_TOP_K: int = 2
+# The size of each text chunk in tokens
+CHUNK_SIZE: int = 512
+# The overlap between adjacent text chunks in tokens
+CHUNK_OVERLAP: int = 50
+
+
+# --- Persistent Storage Paths (using pathlib for robust path handling) ---
+ROOT_PATH: Path = Path(__file__).parent.parent
+DATA_PATH: Path = ROOT_PATH / "data/"
+EMBEDDING_CACHE_PATH: Path = ROOT_PATH / "local_storage/embedding_model/"
+VECTOR_STORE_PATH: Path = ROOT_PATH / "local_storage/vector_store/"
--- src/model_loader.py
+++ src/model_loader.py
@@ -11,6 +11,13 @@ from src.config import (
     # LLM_REPETITION_PENALTY
 )
 
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+
+from src.config import (
+    EMBEDDING_MODEL_NAME,
+    EMBEDDING_CACHE_PATH,
+)
+
 
 # Load environment variables from the .env file
 load_dotenv()
@@ -34,4 +41,16 @@ def initialise_llm() -> Groq:
         # max_tokens=LLM_MAX_NEW_TOKENS,
         # temperature=LLM_TEMPERATURE,
         # top_p=LLM_TOP_P,
+    )
+
+
+def get_embedding_model() -> HuggingFaceEmbedding:
+    """Initialises and returns the HuggingFace embedding model"""
+
+    # Create the cache directory if it doesn't exist
+    EMBEDDING_CACHE_PATH.mkdir(parents=True, exist_ok=True)
+
+    return HuggingFaceEmbedding(
+        model_name=EMBEDDING_MODEL_NAME,
+        cache_folder=EMBEDDING_CACHE_PATH.as_posix()
     )
